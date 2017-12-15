58b82afc2b204dbf78fdb0ad1cc51e3b0ef4b54f
gaarangoa <gaarangoa@gmail.com>
2017-12-14 22:38:43 -0500

    fix

--- API/API.py
+++ API/API.py
@@ -18,7 +18,7 @@ class PredictSentiment(Resource):
         args = parser.parse_args()
         return p.pred(query = args)
 
-api.add_resource(PredictSentiment, '/predict/sentiment')
+api.add_resource(PredictSentiment, '/')
 
 if __name__ == '__main__':
     app.run(debug=True)
