4cfa22fdac51afbde6d23a423f00fac4c438da50
gaarangoa <gaarangoa@gmail.com>
2017-12-14 22:09:21 -0500

    fix

--- API/API.py
+++ API/API.py
@@ -2,8 +2,8 @@ from flask import Flask
 from flask_restful import Resource, Api, reqparse, abort, Resource
 import predict
 
-local_dir = "/var/www/html/bitpred/API";
-p = predict.Predict(model_name = local_dir+'/model.hdf5')
+# local_dir = "/var/www/html/bitpred/API";
+# p = predict.Predict(model_name = local_dir+'/model.hdf5')
 
 parser = reqparse.RequestParser()
 parser.add_argument("task")
@@ -14,9 +14,9 @@ api = Api(app)
 class PredictSentiment(Resource):
     def get(self):
         return {'info': 'This API is designed to predict the sentiment (bearish/bullish) of bitcoin data'}
-    def post(self):
-        args = parser.parse_args()
-        return p.pred(query = args)
+    # def post(self):
+    #     args = parser.parse_args()
+    #     return p.pred(query = args)
 
 api.add_resource(PredictSentiment, '/predict/sentiment')
 
