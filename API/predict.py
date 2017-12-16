67dc571afe570949d9a997e6fd2f7e300250d08c
gaarangoa <gaarangoa@gmail.com>
2017-12-16 02:31:00 -0500

    fix

--- API/predict.py
+++ API/predict.py
@@ -21,8 +21,8 @@ class Predict():
         stock = []
         price = []
         for j in query['stocks']:
-            stock.append([10*j['volume'], j['high']-j['low']])
-            price.append([j['high'], j['low']])
+            stock.append( [10*j['volume'], j['high']-j['low']] )
+            price.append( [j['high'], j['low']] )
 
         padded_stock = np.array(pad_sequences([stock], maxlen=self.max_length_stock_series, padding='pre'))
         padded_price = np.array(pad_sequences([price], maxlen=self.max_length_price_series, padding='pre'))
--- API/start.wsgi
+++ API/start.wsgi
@@ -1,3 +1,3 @@
 import sys
 sys.path.insert(0, '/var/www/html/bitpred/')
-from API import app as application
+from API.API import app as application
