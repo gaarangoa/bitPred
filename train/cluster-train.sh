ad35e8e563a9bd649a51ded63ebf87170f08baf4
gaarangoa <gaarangoa@gmail.com>
2018-01-23 18:29:06 -0500

    added tensorflow

--- train/cluster-train.sh
+++ train/cluster-train.sh
@@ -14,7 +14,7 @@ echo "Allocated GPU with ID $CUDA_VISIBLE_DEVICES"
 source /home/gustavo1/deep_learning/bitPred/env/bin/activate
 # source activate machine-learning
 
-module load theano
+module load theano tensorflow
 
 # change to the directory where the training scripts are
 cd /work/newriver/gustavo1/deepLearning/bitPred/train/
