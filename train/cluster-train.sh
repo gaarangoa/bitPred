002f6ce0838bec344ce6b23417bee6f657136be8
gaarangoa <gaarangoa@gmail.com>
2018-01-23 18:16:45 -0500

    updatex

--- train/cluster-train.sh
+++ train/cluster-train.sh
@@ -11,15 +11,10 @@ echo "Allocated GPU with ID $CUDA_VISIBLE_DEVICES"
 # export PYTHONNOUSERSITE=True
 # source /work/newriver/gustavo1/deepLearning/bitPredEnv/bin/activate
 
-# dont use the local installed modules
-export PYTHONNOUSERSITE=True
+source /home/gustavo1/deep_learning/bitPred/env/bin/activate
+# source activate machine-learning
 
-# activate the virtual environment in conda
-module load anaconda2 
-source activate machine-learning
-
-# load modules
-module load gcc/5.4.0 cuda theano nccl anaconda2
+module load theano
 
 # change to the directory where the training scripts are
 cd /work/newriver/gustavo1/deepLearning/bitPred/train/
