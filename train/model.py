d043cf3804a2f5d49fe849e3ddec7a6051c12573
gaarangoa <gaarangoa@gmail.com>
2018-02-07 19:33:12 -0500

    update moel

--- train/model.py
+++ train/model.py
@@ -56,7 +56,7 @@ class Train():
 
         # dataset and class labels
         docs = data['text']
-        raw_labels = np.array( [ i[1] for i in data['regression'] ])
+        raw_labels = np.array( [ i[2] for i in data['regression'] ])
         # raw_labels_2 = np.array( [ i[2] for i in data['regression'] ])
         # raw_labels_3 = np.array( [ i[3] for i in data['regression'] ])
 
